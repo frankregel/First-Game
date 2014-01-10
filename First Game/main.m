36a0679936726d3c3cd12c667dd2ccb2ac08d03b
Frank Regel <frankregel@frankregelsmbp.fritz.box>
2014-01-10 14:37:35 +0100

    Initial Commit

---
+++ First Game/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  First Game
+//
+//  Created by Frank Regel on 10.01.14.
+//  Copyright (c) 2014 Frank Regel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
